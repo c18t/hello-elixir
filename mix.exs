dbfe5b8766c7bf4b6ec788fdf5b6b3c98d27d088
鵜巷 <u@chimata.org>
2018-05-20 22:41:24 +0900

    supervisor and application

--- mix.exs
+++ mix.exs
@@ -13,9 +13,8 @@ defmodule KV.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    [applications: [],
+     mod: {KV, []}]
   end
 
   # Run "mix help deps" to learn about dependencies.
